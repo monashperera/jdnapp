8056f911ec5a9d29db224d8779e4544305eaa8ca
monashperera <monashperera@gmail.com>
2017-11-03 17:50:41 +0530

    sss

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { RestServiceProvider } from '../../providers/rest-service/rest-service';
 import { NavController } from 'ionic-angular';
-
+import { Http, RequestOptions, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+//import{submitform} from'/';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -10,8 +12,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   public resData;
+  data: any;
 
-  constructor(public navCtrl: NavController, public restServiceProvider: RestServiceProvider) {
+  constructor(public navCtrl: NavController, public restServiceProvider: RestServiceProvider,public http: Http) {
     restServiceProvider.load()
     .then(data => {
       console.log(data);
@@ -20,13 +23,27 @@ export class HomePage {
     })
 
   }
-  postReq(){
-    this.restServiceProvider.postr()
-    .then(data => {
-      console.log(data);
-      this.resData = data;
+  postRequest() {
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    let options = new RequestOptions({ headers: headers });
 
-    })
+    let postParams = {
+      security: 'LL9Ny4IF',
+      email: 'you@yourmail.com',
+      password: '123124'
+    };
+    alert(document.getElementById('email').innerHTML);
+    console.log(postParams)
+
+
+    this.http.post("https://api.pathola.com/freelance_login_submit.php",postParams, options)
+      .subscribe(data => {
+        console.log(data['_body']);
+      }, error => {
+        console.log(error);// Error getting the data
+      });
   }
 
 }
